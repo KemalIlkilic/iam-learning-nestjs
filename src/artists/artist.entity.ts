12cf27c3bbf25323a2e7c9064dcac4a8e14a0320
Kemal Onur İlkılıç <kemalonur.ilkilic@bahcesehir.edu.tr>
2025-03-10 23:55:51 +0300

    feat: add artist and user entities with relationships for database schema

---
+++ src/artists/artist.entity.ts
@@ -0,0 +1,19 @@
+import { User } from 'src/users/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('artists')
+export class Artist {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @OneToOne(() => User)
+  //Decorated with @JoinColumn() to specify that this side of the relationship owns the foreign key.
+  @JoinColumn()
+  user: User;
+}
